90c2e7d11c95acc578d61b0082083d3b65b6bc43
Akhmadjon Usmonov <qazakhi515@gmail.com>
2025-12-27 23:45:33 +0900

    fix: developing portfolio part-1

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 console.log('Web Serverni boshlash');
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
 
 // 1 Kirish code 
 app.use(express.static("public"));
@@ -14,9 +26,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
+app.post("/create-item", (req, res) => {
+
+});
+
+app.get("/author", (req, res) => {
+res.render("author",{ user: user });
+});
+
 app.get("/", function (req, res) {
     res.render('harid')
-})
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
--- train.js
+++ train.js
@@ -1,3 +1,42 @@
-// Task b
 // Task a 
-console.log('practice makes more perfect')
+function countLetter(harf, soz) {
+  let count = 0;
+
+  for (let i = 0; i < soz.length; i++) {
+    if (soz[i] === harf) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+console.log(countLetter("m", "muhammadamin"));
+console.log(countLetter("a", "abama"));
+console.log(countLetter("k", "kalaka"));
+// 
+// console.log('Jeck Ma maslahatlari');
+// const list = [
+//     "yahshi talaba boling", // 0-20 
+//      "togri boshlig tanlang va koproq hato qiling", // 20-30
+//      "uzingizga ishlashingizni boshlang", // 30-40
+//      "siz kuchli bolgan narsalarni qiling", // 40-50
+//      "yoshlarga investitsiya qiling", //50-60
+//      "endi dam oling, foydasi yoq endi", // 60>>
+// ];
+// function maslahatBering(a, callback) {
+//  if (typeof a !== "number") callback ("insert a number", null);
+//  else if (a <= 20) callback(null, list[0]); 
+//  else if (a > 20 && a <= 30) callback(null, list[1]); 
+//  else if (a > 30 && a <= 40) callback(null, list[2]); 
+//  else if (a > 40 && a <= 50) callback(null, list[3]); 
+//  else if (a < 50 && a <= 60) callback(null, list[4]);
+//  else {
+//     callback(null, list[5]);
+//  } 
+// }
+
+// maslahatBering(31, (err, data) => {
+//     if(err) console.log("ERROR:", err);
+//     console.log('javob:', data);
+// });
