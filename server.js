091f05afebe31f72676150bcb47503feef0407c9
Akhmadjon Usmonov <qazakhi515@gmail.com>
2025-12-31 14:06:39 +0900

    fix: mongo db connected

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
-console.log('Web Serverni boshlash');
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
+let db;
+const connectionString = 
+"mongodb+srv://qazakhi515_db_user:G970j5Eovi2m5vN7@cluster0.quqkbut.mongodb.net/Reja?retryWrites=true&w=majority";
+
+mongodb.connect(
+    connectionString, 
+ {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+ }, 
+ (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+  else {
+     console.log("MongoDB connection succeed");
+     module.exports = client;
+     const app = require("./app");
+     const server = http.createServer(app);
+     let PORT = 3000;
+     server.listen(PORT, function() {
+     console.log(
+         `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+  });
+ }
+
 });
 
-
-// 1 Kirish code 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2 Session code
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-
-});
-
-app.get("/author", (req, res) => {
-res.render("author",{ user: user });
-});
-
-app.get("/", function (req, res) {
-    res.render('harid')
-});
-
-const server = http.createServer(app);
-let PORT = 3004;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}`);
-});
