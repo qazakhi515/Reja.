1d5163aaa41f0abe51022a6e087080559c99b151
Akhmadjon Usmonov <qazakhi515@gmail.com>
2025-12-31 15:23:57 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -17,11 +17,34 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
+    console.log('user entered /create-item')
+    console.log(req.body);
 
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja}, (err, data) =>{
+        if(err) {
+          console.log(err);
+          res.end("something went wrong");
+        } else {
+          res.end("successfully added");
+        }
+    });
 });
 
 app.get("/", function (req, res) {
-    res.render("reja")
+    console.log('user entered /')
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err){
+            console.log(err);
+            res.end("something went wrong");
+        } else{
+            console.log(data);
+             res.render("reja", {items: data});
+        }
+    });
+   
 });
 
 module.exports = app;
