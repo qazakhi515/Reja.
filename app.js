21bc948d1a956907d60b7bbde199d08c5066d8ce
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-01-01 18:57:19 +0900

    fix: upload

--- app.js
+++ app.js
@@ -17,17 +17,13 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log('user entered /create-item')
+    console.log('user entered /create-item');
     console.log(req.body);
 
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja}, (err, data) =>{
-        if(err) {
-          console.log(err);
-          res.end("something went wrong");
-        } else {
-          res.end("successfully added");
-        }
+        console.log(data.ops);
+        res.json(data.ops[0]);
     });
 });
 
@@ -36,11 +32,11 @@ app.get("/", function (req, res) {
     db.collection("plans")
     .find()
     .toArray((err, data) => {
-        if(err){
+        if(err) {
             console.log(err);
             res.end("something went wrong");
-        } else{
-            console.log(data);
+        } else {
+           
              res.render("reja", {items: data});
         }
     });
